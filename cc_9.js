f7d5c7fd8044afd7fbe64313882d03ca56d1abba
Missy Hominsky <hominsky@2usf.edu>
2025-02-22 01:09:05 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -21,3 +21,29 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 
 console.log(emp1.getDetails()); 
 console.log(emp1.calculateAnnualSalary()); 
+
+//Task 2 - Created Manager Class with Inheritance
+
+class Manager extends Employee{ //Creating class Mangager 
+    constructor(name, id, department, salary, teamSize) { //new properties for constructor 
+        super(name, id, department, salary); 
+        this.teamSize = teamSize; 
+    };
+
+    getDetails() { //new getdetaisl tab after teamsize was added 
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`; 
+    };
+
+    calculateBonus() { //calculateBonus method setup
+        return super.calculateAnnualSalary() * 0.1; //bonus = ten percent of annual salary
+    };
+
+    calculateAnnualSalary() { 
+        return super.calculateAnnualSalary() + this.calculateBonus(); //employee's annual salary + calculateBonus()
+    };
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); 
+
+console.log(mgr1.getDetails()); 
+console.log(mgr1.calculateBonus()); 
